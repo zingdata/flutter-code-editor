48365ca063d5b240671dbadad27b8d4f4a1374af
Raza Abbas <44662708+razankv13@users.noreply.github.com>
2024-02-29 19:36:10 +0500

    allow dot for suggestions

--- lib/src/code/reg_exp.dart
+++ lib/src/code/reg_exp.dart
@@ -1,3 +1,4 @@
 class RegExps {
-  static final wordSplit = RegExp('[^_a-zA-Z0-9]+');
+  static final wordSplit = RegExp('[^._a-zA-Z0-9]+');
 }
+// added dot to fix this https://gitlab.com/getzingdata/mobileappflutter/-/issues/886
